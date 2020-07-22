eac5e0b1aff4b8467eb9a842eccfe207ba5ed1bd
jym0404 <49983304+jym0404@users.noreply.github.com>
2020-07-22 16:48:40 +0900

    Create js.js

---
+++ js.js
@@ -0,0 +1,38 @@
+var listitems = document.querySelectorAll('div');
+	for(var i = 0;i<listitems.length; i++) {
+	
+	listitems[i].addEventListener('click',
+	(e)=>{
+		
+		var daedap = prompt("히든코드는?")
+		if(daedap == "jym"){
+			alert("맞히셨습니다."+"\n"+"축하드립니다.");
+		}
+		else if(daedap == null){
+			
+			
+		}
+		else{
+			alert("틀리셨습니다."+"\n"+"다시 도전하세요.");
+			
+		}
+
+	}
+
+  );
+}
+var a
+
+	/*listitems.addEventListener('click',
+	(e)=>{
+		var daedap = prompt("히든코드는?")
+		if(daedap == jym){
+			alert("맞히셨습니다."+"\n"+"축하드립니다.");
+		}
+		else if(daedap != jym){
+			alert("틀리셨습니다."+"\n"+"다시 도전하세요.")
+		}
+
+	}
+
+  );*/
